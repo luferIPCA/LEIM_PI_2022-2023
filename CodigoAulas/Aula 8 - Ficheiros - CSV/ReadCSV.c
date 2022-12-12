ba9f7f18a8ab3033e49f17686d3fd0946ca754c7
lufer <lufer@ipca.pt>
2022-12-12 21:56:03 +0000

    Read CSV Files

--- CodigoAulas/Aula 8 - Ficheiros - CSV/ReadCSV.c
+++ CodigoAulas/Aula 8 - Ficheiros - CSV/ReadCSV.c
@@ -10,41 +10,25 @@
 #include<string.h>
 #include<stdbool.h>
 
-#define MAXCHAR 1000
-#define MAXVALS 100
+
 #pragma warning (disable: 4996)
 
-typedef struct Valor {
-    char data[30];
-    double v1;
-    double v2;
-    double v3;
-    double v4;
-    double v5;
-}Valor;
+#include "Var.h"
 
 int main() {
 
     FILE* fp;
-    char row[MAXCHAR];
+    char row[MAXCHAR];  //auxiliar para guardar cada linha do ficheiro
     char* token;
 
+    //Estrutura para guardar dados do ficheiro
     Valor valores[MAXVALS];
     int totalValores = 0;
 
 #pragma region LerCSV
 
-    //fp = fopen("Dados.csv", "r");
-    ////testar se conseguiu abrir
-
-    //while (feof(fp) != true)
-    //{
-    //    fgets(row, MAXCHAR, fp);
-    //    printf("Row: %s", row);
-    //}
-
-    //fclose(fp);
-
+    bool b = LeDadosCSV("Dados.csv");
+    
 #pragma endregion
 
 #pragma region AnalisaCSV
@@ -72,19 +56,26 @@ int main() {
         while (token != NULL)
         {
             printf("Token: %s\n", token);
-            if (t == 0) {
-                strcpy(aux.data, token); t++; continue;
+            switch (t) {
+            case 0:
+                strcpy(aux.data, token);
+                break;
+            case 1: 
+                aux.high = atof(token); 
+                break;
+            case 2: 
+                aux.low = atof(token);
+                break;
+
+            case 3: 
+                aux.open = atof(token);
+                break;
+            case 4: 
+                aux.volume = atof(token);
+                break;
             }
-            if (t == 1) {
-                aux.v1 = atof(token); t++; continue;
-            }
-            if (t == 2) {
-                aux.v2 = atof(token); t++;  continue;
-            }
-            //fazer o restantes campos
-            //
-            token = strtok(NULL, ",");
-            
+            t++;
+            token = strtok(NULL, ",");     
         }
         //guarda o aux no array
         valores[totalValores] = aux;
